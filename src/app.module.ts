d5223843f0110d4b4102206e2ee2b0f60ff8e53e
armando_rojas <armando_rojas_10@hotmail.com>
2022-11-14 20:30:31 +0000

    config .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,18 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
   imports: [
-
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGO_DB),
     PokemonModule,
     CommonModule,
     SeedModule
@@ -20,4 +24,8 @@ import { SeedModule } from './seed/seed.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+    console.log(process.env.PORT) 
+  }
+}
